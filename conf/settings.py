c647f2869b72b8b348a4c3ba881d06ccc5da74a3
pincoin <pincoins@gmail.com>
2019-08-16 02:49:04 +0900

    - css fix

--- conf/settings.py
+++ conf/settings.py
@@ -104,7 +104,6 @@ STATIC_URL = '/assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'conf', 'static'),
-    os.path.join(BASE_DIR, 'golf', 'static'),
 ]
 
 # Media files (Uploaded files)
