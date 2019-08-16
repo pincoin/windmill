25d8fdf038ca0eb57279f789f7e3e1c6341bea0b
pincoin <pincoins@gmail.com>
2019-08-16 19:38:43 +0900

    - reset migrations

--- member/urls.py
+++ member/urls.py
@@ -2,3 +2,47 @@ app_name = 'member'
 
 urlpatterns = [
 ]
+
+'''
+# Account
+path('login/',
+     views.MemberLoginView.as_view(), name="account_login"),
+path('logout/',
+     views.MemberLogoutView.as_view(), name="account_logout"),
+path('signup/',
+     views.MemberSignupView.as_view(), name="account_signup"),
+path('inactive/',
+     views.MemberAccountInactiveView.as_view(), name="account_inactive"),
+path('unregister/',
+     views.MemberUnregisterView.as_view(), name="account_unregister"),
+
+# Password Change
+path('password/change/',
+     views.MemberPasswordChangeView.as_view(), name="account_change_password"),
+path('password/set/',
+     views.MemberPasswordSetView.as_view(), name="account_set_password"),
+
+# Password Reset
+path('password/reset/',
+     views.MemberPasswordReset.as_view(), name="account_reset_password"),
+path('password/reset/done/',
+     views.MemberPasswordResetDoneView.as_view(), name="account_reset_password_done"),
+re_path(r'^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$',
+        views.MemberPasswordResetFromKeyView.as_view(), name="account_reset_password_from_key"),
+path('password/reset/key/done/',
+     views.MemberPasswordResetFromKeyDoneView.as_view(), name="account_reset_password_from_key_done"),
+
+# Email Confirmation
+path('confirm-email/',
+     views.MemberEmailVerificationSentView.as_view(), name="account_email_verification_sent"),
+re_path(r'^confirm-email/(?P<key>[-:\w]+)/$',
+        views.MemberConfirmEmailView.as_view(), name="account_confirm_email"),
+path('email/',
+     views.MemberEmailView.as_view(), name="account_email"),
+
+# Profile
+path('profile/',
+     views.MemberProfileView.as_view(), name="account_profile"),
+path('change-name/',
+     views.MemberLoginView.as_view(), name="account_change_name"),
+     '''
