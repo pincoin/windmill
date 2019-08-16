6201f17624eddc19378b25758f1a2b29e3f2abb0
pincoin <pincoins@gmail.com>
2019-08-16 15:59:55 +0900

    - local.py

--- conf/settings/local.py
+++ conf/settings/local.py
@@ -3,8 +3,7 @@ import os
 
 from django.utils.translation import ugettext_lazy as _
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from . import BASE_DIR
 
 # Secret settings
 secret = json.loads(open(os.path.join(BASE_DIR, 'secret.json')).read())
