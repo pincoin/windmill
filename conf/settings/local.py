06f45d9959d79aee27ce1069c4db1f650ffe9d54
pincoin <pincoins@gmail.com>
2019-08-16 16:04:52 +0900

    - base settings

--- conf/settings/local.py
+++ conf/settings/local.py
@@ -1,87 +1,8 @@
-import json
-import os
-
 from django.utils.translation import ugettext_lazy as _
 
-from . import BASE_DIR
+from .base import *
 
-# Secret settings
-secret = json.loads(open(os.path.join(BASE_DIR, 'secret.json')).read())
-
-DEBUG = secret['DEBUG']
-SECRET_KEY = secret['SECRET_KEY']
-ALLOWED_HOSTS = secret['ALLOWED_HOSTS']
-DATABASES = secret['DATABASES']
-
-# Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-]
-
-INSTALLED_APPS += [
-    'allauth',
-    'allauth.account',
-]
-
-INSTALLED_APPS += [
-    'member',
-    'golf',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'conf.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'conf', 'templates'), ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'conf.wsgi.application'
-
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+DEBUG = True
 
 # Internationalization
 LANGUAGE_CODE = 'ko-KR'
