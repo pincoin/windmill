6080f4695f43d51bfc444abb796f5db0d684981d
pincoin <pincoins@gmail.com>
2019-08-17 23:28:20 +0900

    - logging

--- conf/settings/local.py
+++ conf/settings/local.py
@@ -32,3 +32,32 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # django.contrib.sites settings for allauth
 SITE_ID = 1
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'conf': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'golf': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'member': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
