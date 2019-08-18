636f9367c19963457d98f9358fc4e266f1c85d68
pincoin <pincoins@gmail.com>
2019-08-18 21:03:37 +0900

    - grey background touch toggle

--- conf/static/conf/js/base.js
+++ conf/static/conf/js/base.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").on('click', function (e) {
+    $('.navbar-burger, #gray-background').on('click', function (e) {
         $(".navbar-burger, .navbar-menu").toggleClass("is-active");
 
         $('html').toggleClass('is-clipped');
