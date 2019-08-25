9dcd259a466e1b6b2bb1fef25457fb7c7240555d
pincoin <pincoins@gmail.com>
2019-08-25 19:25:11 +0900

    - staff booking list

--- golf/views.py
+++ golf/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import mixins as auth_mixins
+from django.utils.translation import ugettext_lazy as _
 from django.views import generic
 
 from . import models
@@ -14,6 +15,11 @@ class AgencyBookingList(auth_mixins.LoginRequiredMixin, generic.ListView):
             .filter(agent__id=self.request.user.id)
         return queryset
 
+    def get_context_data(self, **kwargs):
+        context = super(AgencyBookingList, self).get_context_data(**kwargs)
+        context['page_title'] = _('Booking management')
+        return context
+
 
 class AgencyBookingCreate(auth_mixins.LoginRequiredMixin, generic.CreateView):
     pass
@@ -28,7 +34,18 @@ class AgencyBookingDelete(auth_mixins.LoginRequiredMixin, generic.DeleteView):
 
 
 class StaffBookingList(auth_mixins.LoginRequiredMixin, generic.ListView):
-    pass
+    template_name = 'golf/staff_booking_list.html'
+    context_object_name = 'booking_list'
+
+    def get_queryset(self):
+        queryset = models.Booking.objects \
+            .select_related('club', 'agency', 'agent')
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(StaffBookingList, self).get_context_data(**kwargs)
+        context['page_title'] = _('Booking management')
+        return context
 
 
 class StaffBookingDetail(auth_mixins.LoginRequiredMixin, generic.DetailView):
