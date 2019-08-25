345c76768c1805c5ac7549b5b186ac6c9c4132aa
pincoin <pincoins@gmail.com>
2019-08-26 01:17:57 +0900

    - search forms

---
+++ golf/forms.py
@@ -0,0 +1,93 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+from . import models
+
+
+class BookingSearchForm(forms.Form):
+    category = forms.ChoiceField(
+        choices=(
+            ('1', _('Booking person'),),
+            ('2', _('Memo'),),
+        ),
+        widget=forms.Select(
+            attrs={
+                'class': 'input',
+                'required': 'True',
+            }
+        )
+    )
+
+    keyword = forms.CharField(
+        label=_('Search word'),
+        widget=forms.TextInput(
+            attrs={
+                'class': 'input',
+                'placeholder': _('Search word'),
+                'required': 'True',
+            }
+        ),
+    )
+
+    def __init__(self, *args, **kwargs):
+        category = kwargs.pop('category', '1')
+        keyword = kwargs.pop('keyword', '')
+
+        super(BookingSearchForm, self).__init__(*args, **kwargs)
+
+        self.fields['category'].initial = category
+        self.fields['keyword'].initial = keyword
+
+
+class BookingStatusSearchForm(forms.Form):
+    status = forms.ChoiceField(
+        choices=models.Booking.STATUS_CHOICES,
+        widget=forms.Select(
+            attrs={
+                'required': 'True',
+            }
+        )
+    )
+
+    def __init__(self, *args, **kwargs):
+        status = kwargs.pop('status', '0')
+
+        super(BookingStatusSearchForm, self).__init__(*args, **kwargs)
+
+        self.fields['status'].initial = status
+
+
+class BookingGolfClubSearchForm(forms.Form):
+    club = forms.ChoiceField(
+        widget=forms.Select(
+            attrs={
+                'required': 'True',
+            }
+        )
+    )
+
+    def __init__(self, *args, **kwargs):
+        club = kwargs.pop('club', '0')
+
+        super(BookingGolfClubSearchForm, self).__init__(*args, **kwargs)
+
+        self.fields['club'].initial = club
+        self.fields['club'].choices = [(c.id, str(c.title)) for c in models.Club.objects.all()]
+
+
+class BookingAgencySearchForm(forms.Form):
+    agency = forms.ChoiceField(
+        widget=forms.Select(
+            attrs={
+                'required': 'True',
+            }
+        )
+    )
+
+    def __init__(self, *args, **kwargs):
+        agency = kwargs.pop('agency', '0')
+
+        super(BookingAgencySearchForm, self).__init__(*args, **kwargs)
+
+        self.fields['agency'].initial = agency
+        self.fields['agency'].choices = [(a.id, str(a.title)) for a in models.Agency.objects.all()]
--- golf/views.py
+++ golf/views.py
@@ -2,12 +2,18 @@ from django.contrib.auth import mixins as auth_mixins
 from django.utils.translation import ugettext_lazy as _
 from django.views import generic
 
+from . import forms
 from . import models
 
 
 class AgencyBookingList(auth_mixins.LoginRequiredMixin, generic.ListView):
     template_name = 'golf/agency_booking_list.html'
     context_object_name = 'booking_list'
+
+    booking_search_form_class = forms.BookingSearchForm
+    booking_status_search_form_class = forms.BookingStatusSearchForm
+    booking_club_search_form_class = forms.BookingGolfClubSearchForm
+    booking_agency_search_form_class = forms.BookingAgencySearchForm
 
     def get_queryset(self):
         queryset = models.Booking.objects \
@@ -18,6 +24,24 @@ class AgencyBookingList(auth_mixins.LoginRequiredMixin, generic.ListView):
     def get_context_data(self, **kwargs):
         context = super(AgencyBookingList, self).get_context_data(**kwargs)
         context['page_title'] = _('Booking management')
+
+        context['booking_search_form'] = self.booking_search_form_class(
+            category=self.request.GET.get('category') if self.request.GET.get('category') else '1',
+            keyword=self.request.GET.get('keyword') if self.request.GET.get('keyword') else '',
+        )
+
+        context['booking_status_search_form'] = self.booking_status_search_form_class(
+            status=self.request.GET.get('status') if self.request.GET.get('status') else '',
+        )
+
+        context['booking_club_search_form'] = self.booking_club_search_form_class(
+            club=self.request.GET.get('club') if self.request.GET.get('club') else '',
+        )
+
+        context['booking_agency_search_form'] = self.booking_agency_search_form_class(
+            agency=self.request.GET.get('agency') if self.request.GET.get('agency') else '',
+        )
+
         return context
 
 
@@ -37,6 +61,11 @@ class StaffBookingList(auth_mixins.LoginRequiredMixin, generic.ListView):
     template_name = 'golf/staff_booking_list.html'
     context_object_name = 'booking_list'
 
+    booking_search_form_class = forms.BookingSearchForm
+    booking_status_search_form_class = forms.BookingStatusSearchForm
+    booking_club_search_form_class = forms.BookingGolfClubSearchForm
+    booking_agency_search_form_class = forms.BookingAgencySearchForm
+
     def get_queryset(self):
         queryset = models.Booking.objects \
             .select_related('club', 'agency', 'agent')
@@ -45,6 +74,24 @@ class StaffBookingList(auth_mixins.LoginRequiredMixin, generic.ListView):
     def get_context_data(self, **kwargs):
         context = super(StaffBookingList, self).get_context_data(**kwargs)
         context['page_title'] = _('Booking management')
+
+        context['booking_search_form'] = self.booking_search_form_class(
+            category=self.request.GET.get('category') if self.request.GET.get('category') else '1',
+            keyword=self.request.GET.get('keyword') if self.request.GET.get('keyword') else '',
+        )
+
+        context['booking_status_search_form'] = self.booking_status_search_form_class(
+            status=self.request.GET.get('status') if self.request.GET.get('status') else '',
+        )
+
+        context['booking_club_search_form'] = self.booking_club_search_form_class(
+            club=self.request.GET.get('club') if self.request.GET.get('club') else '',
+        )
+
+        context['booking_agency_search_form'] = self.booking_agency_search_form_class(
+            agency=self.request.GET.get('agency') if self.request.GET.get('agency') else '',
+        )
+
         return context
 
 
