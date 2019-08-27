7cb350cccc7613a16ed1a027db3a8c4d5d7b5f2f
pincoin <pincoins@gmail.com>
2019-08-27 15:34:37 +0900

    - booking.js

--- conf/settings/local.py
+++ conf/settings/local.py
@@ -24,6 +24,7 @@ STATIC_URL = '/assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'conf', 'static'),
+    os.path.join(BASE_DIR, 'golf', 'static'),
 ]
 
 # Media files (Uploaded files)
