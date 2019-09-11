d158cd37ee82667940c94f0f42fb7ae290196085
pincoin <pincoins@gmail.com>
2019-09-11 15:13:10 +0900

    - js refactoring

--- golf/static/golf/js/booking.js
+++ golf/static/golf/js/booking.js
@@ -1,41 +1,78 @@
-$(document).ready(function () {
+function get_fee() {
     let club = $('#id_club');
     let agency = $('#id_agency');
     let slot = $('#id_slot');
     let round_date = $('#id_round_date');
-    let round_time_hour = $('#id_round_time_hour');
-    let people = $('#id_people');
     let round_date_info = $('#id_round_date_info');
+    let fee = $('#id_fee');
+    let people = $('#id_people');
+
+    if (club.val() && round_date.val() && round_time_hour.val()) {
+        $.ajax({
+            url: '/golf/api/fee/',
+            type: 'post',
+            dataType: 'json',
+            data: {
+                'club_id': parseInt(club.val()),
+                'agency_id': parseInt(agency.val()),
+                'round_date': round_date.val().substring(0, 10),
+                'slot': parseInt(slot.val())
+            },
+            beforeSend: function (xhr, settings) {
+                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader('X-CSRFToken', csrftoken);
+                }
+            }
+        }).done(function (data, textStatus, jqXHR) {
+            fee.val(data.fee * people.val());
+            round_date_info.val(data.season + '/' + data.day_of_week);
+        }).fail(function (data, textStatus, errorThrown) {
+            round_date_info.val(data.responseJSON['__all__'][0]['message']);
+        });
+    }
+}
+
+function reset_round_time_hour() {
+    let slot = $('#id_slot');
+    let round_time_hour = $('#id_round_time_hour');
+
+    round_time_hour.empty();
+
+    switch (parseInt(slot.val())) {
+        case 0:
+            round_time_hour.append('<option value="6">06</option>'
+                + '<option value="7">07</option>'
+                + '<option value="8">08</option>'
+                + '<option value="9">09</option>'
+                + '<option value="10">10</option>'
+                + '<option value="11">11</option>');
+            break;
+        case 1:
+            round_time_hour.append('<option value="11">11</option>'
+                + '<option value="12">12</option>'
+                + '<option value="13">13</option>'
+                + '<option value="14">14</option>');
+            break;
+        case 2:
+            round_time_hour.append('<option value="15">15</option>');
+            break;
+        case 3:
+            round_time_hour.append('<option value="16">16</option>'
+                + '<option value="17">17</option>'
+                + '<option value="18">18</option>'
+                + '<option value="19">19</option>'
+                + '<option value="20">20</option>');
+            break;
+    }
+}
+
+$(document).ready(function () {
+    let club = $('#id_club');
+    let slot = $('#id_slot');
+    let round_date = $('#id_round_date');
+    let people = $('#id_people');
 
     let tee_off_time_form = $('#id_tee_off_time_form');
-
-    let fee = $('#id_fee');
-
-    function get_fee() {
-        if (club.val() && round_date.val() && round_time_hour.val()) {
-            $.ajax({
-                url: '/golf/api/fee/',
-                type: 'post',
-                dataType: 'json',
-                data: {
-                    'club_id': parseInt(club.val()),
-                    'agency_id': parseInt(agency.val()),
-                    'round_date': round_date.val().substring(0, 10),
-                    'slot': parseInt(slot.val())
-                },
-                beforeSend: function (xhr, settings) {
-                    if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                        xhr.setRequestHeader('X-CSRFToken', csrftoken);
-                    }
-                }
-            }).done(function (data, textStatus, jqXHR) {
-                fee.val(data.fee * people.val());
-                round_date_info.val(data.season + '/' + data.day_of_week);
-            }).fail(function (data, textStatus, errorThrown) {
-                round_date_info.val(data.responseJSON['__all__'][0]['message']);
-            });
-        }
-    }
 
     round_date.datepicker({
         showAnim: '', // turn off animation
@@ -58,35 +95,7 @@ $(document).ready(function () {
     });
 
     slot.on('change', function (e) {
-        round_time_hour.empty();
-
-        switch (parseInt(slot.val())) {
-            case 0:
-                round_time_hour.append('<option value="6">06</option>'
-                    + '<option value="7">07</option>'
-                    + '<option value="8">08</option>'
-                    + '<option value="9">09</option>'
-                    + '<option value="10">10</option>'
-                    + '<option value="11">11</option>');
-                break;
-            case 1:
-                round_time_hour.append('<option value="11">11</option>'
-                    + '<option value="12">12</option>'
-                    + '<option value="13">13</option>'
-                    + '<option value="14">14</option>');
-                break;
-            case 2:
-                round_time_hour.append('<option value="15">15</option>');
-                break;
-            case 3:
-                round_time_hour.append('<option value="16">16</option>'
-                    + '<option value="17">17</option>'
-                    + '<option value="18">18</option>'
-                    + '<option value="19">19</option>'
-                    + '<option value="20">20</option>');
-                break;
-        }
-
+        reset_round_time_hour();
         get_fee();
     });
 
@@ -95,11 +104,8 @@ $(document).ready(function () {
     });
 
     $(document).on('click', '#id_tee_off_time_add', function (e) {
-        console.log($(this).data('booking_uuid'));
-
         let status_text = $(this).data('status_text');
         let remove_text = $(this).data('remove_text');
-
 
         $.ajax({
             url: '/golf/api/tee-off-time/add/',
